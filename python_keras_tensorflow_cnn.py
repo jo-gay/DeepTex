e292323a501ee400834fbb1a8a869cc980751420
jo-gay <joanna.gay@gmail.com>
2018-11-08 10:16:03 +0100

    normalisation

--- python_keras_tensorflow_cnn.py
+++ python_keras_tensorflow_cnn.py
@@ -15,7 +15,7 @@ from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
 from keras import optimizers
 from keras.engine.topology import Layer
-filedir = 'data/'
+filedir = '../slides/'
 
 def main():
      res = runImageClassification()
@@ -68,60 +68,60 @@ def myGetModel(x_tr,y_tr, x_va,y_va, x_te,y_te):
     num_classes = 2
     cNN = Sequential()
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(32, kernel_size=(3, 3), activation='sigmoid', input_shape=(1,80,80), padding ="same", trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(32, kernel_size=(3, 3), activation='sigmoid', input_shape=(1,80,80), padding ="same"))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(32, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(32, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     cNN.add(MaxPooling2D(pool_size=(2, 2)))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(64, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(64, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(64, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(64, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     cNN.add(MaxPooling2D(pool_size=(2, 2)))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(128, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(128, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(128, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(128, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(128, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(128, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     cNN.add(MaxPooling2D(pool_size=(2, 2)))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid'))
+ #   cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     cNN.add(MaxPooling2D(pool_size=(2, 2)))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid'))
+#    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     
     cNN.add(ZeroPadding2D((1,1)))
-    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid', trainable = False))
-    cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
+    cNN.add(Conv2D(256, kernel_size=(3, 3), activation='sigmoid'))
+ #   cNN.add(Conv2D(1, kernel_size=(1, 1), activation='linear'))
     cNN.add(MaxPooling2D(pool_size=(2, 2)))
     
     cNN.add(Flatten())
@@ -135,10 +135,10 @@ def myGetModel(x_tr,y_tr, x_va,y_va, x_te,y_te):
     return cNN
     
 def myFitModel(cNN,x_tr,y_tr, x_va,y_va, x_te,y_te):
-    path = "C:/Users/Bulb/Documents/Teknisk Fysik/15hp project/weights-best.hdf5"
+    path = "weights-best.hdf5"
     model_checkpoint = ModelCheckpoint(filepath = path, monitor='val_acc', verbose=1, save_best_only=True, mode='auto', period=1)
     early_stopping = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=0, mode='auto', baseline=None)
-    cNN.fit(x_tr, y_tr, epochs = 1, batch_size = 100, validation_data = (x_va, y_va),callbacks = [model_checkpoint, early_stopping])
+    cNN.fit(x_tr, y_tr, epochs = 10, batch_size = 100, validation_data = (x_va, y_va),callbacks = [model_checkpoint, early_stopping])
     cNN.load_weights(path)
     return cNN
     
@@ -150,93 +150,98 @@ def runImageClassification(getModel=None,fitModel=None,seed=7):
     print("Preparing data...")
     class_names=['healthy','cancer']
         
-    filedir = 'data/'
-    fold1tr=[[3,4,5,6],[37,38,12]]
+    filedir = '../slides/'
+    #fold1tr=[[3,4,5,6],[37,38,12]]
     fold2tr=[[3,4,5,6],[36,37,38]]
-    fold3tr=[[3,4,5,6],[12,36]]
-    fold1te=[[7,8],[36]]
+    #fold3tr=[[3,4,5,6],[12,36]]
+    #fold1te=[[7,8],[36]]
     fold2te=[[7,8],[12]]
-    fold3te=[[7,8],[37,38]]
+    #fold3te=[[7,8],[37,38]]
 
     # Extracting data from files
-    data_fold1_tr=[]
-    labels_fold1_tr=[]
+    data_fold2_tr=[]
+    labels_fold2_tr=[]
     
-    data_fold1_te=[]
-    labels_fold1_te=[]
+    data_fold2_te=[]
+    labels_fold2_te=[]
     
     # training and validation data
     #%%
-    for g in fold1tr[0]:
+    for g in fold2tr[0]:
         filename = filedir+'glass'+str(g)+'.hdf5'
         f = h5py.File(filename, 'r')
         
         a_group_key = list(f.keys())[0]
         d=list(f[a_group_key])
-        data_fold1_tr += d
-        labels_fold1_tr += list(np.zeros(len(d)))
+        data_fold2_tr += d
+        labels_fold2_tr += list(np.zeros(len(d)))
             
-    for g in fold1tr[1]:
+    for g in fold2tr[1]:
         filename = filedir+'glass'+str(g)+'.hdf5'
         f = h5py.File(filename, 'r')
         
         a_group_key = list(f.keys())[0]
         d=list(f[a_group_key])
-        data_fold1_tr += d
-        labels_fold1_tr += list(np.ones(len(d)))
+        data_fold2_tr += d
+        labels_fold2_tr += list(np.ones(len(d)))
         
         # test data
-    for g in fold1te[0]:
+    for g in fold2te[0]:
         filename = filedir+'glass'+str(g)+'.hdf5'
         f = h5py.File(filename, 'r')
         
         a_group_key = list(f.keys())[0]
         d=list(f[a_group_key])
-        data_fold1_te += d
-        labels_fold1_te += list(np.zeros(len(d)))
+        data_fold2_te += d
+        labels_fold2_te += list(np.zeros(len(d)))
             
-    for g in fold1te[1]:
+    for g in fold2te[1]:
         filename = filedir+'glass'+str(g)+'.hdf5'
         f = h5py.File(filename, 'r')
         
         a_group_key = list(f.keys())[0]
         d=list(f[a_group_key])
-        data_fold1_te += d
-        labels_fold1_te += list(np.ones(len(d)))
+        data_fold2_te += d
+        labels_fold2_te += list(np.ones(len(d)))
        
     #%%    
     # Shuffling data and partitioning into training, testing, and validation sets    
-    nSamples=len(labels_fold1_tr)
+    nSamples=len(labels_fold2_tr)
     tr_perc=.80
     va_perc=.20
     
     seed = 1
     random.seed(seed)
-    random.shuffle(labels_fold1_tr)
+    random.shuffle(labels_fold2_tr)
     random.seed(seed)
-    random.shuffle(data_fold1_tr)
+    random.shuffle(data_fold2_tr)
 
     
     tr=round(nSamples*tr_perc)
     va=round(nSamples*va_perc)
         
-    x_tr = np.asarray(data_fold1_tr[0:tr])
-    x_va = np.asarray(data_fold1_tr[tr:nSamples])
-    x_te = np.asarray(data_fold1_te)
-    y_tr = np.asarray(labels_fold1_tr[0:tr])
-    y_va = np.asarray(labels_fold1_tr[tr:nSamples])
-    y_te = np.asarray(labels_fold1_te)
+    x_tr = np.asarray(data_fold2_tr[0:tr])
+    x_va = np.asarray(data_fold2_tr[tr:nSamples])
+    x_te = np.asarray(data_fold2_te)
+    y_tr = np.asarray(labels_fold2_tr[0:tr])
+    y_va = np.asarray(labels_fold2_tr[tr:nSamples])
+    y_te = np.asarray(labels_fold2_te)
 
     # converting vectors to numpy arrays
     y_tr = np_utils.to_categorical(y_tr, 2)
     y_va = np_utils.to_categorical(y_va, 2)
     y_te = np_utils.to_categorical(y_te, 2)
-    x_tr = x_tr.astype('float32')
-    x_va = x_va.astype('float32')
-    x_te= x_te.astype('float32')
-    x_tr /= 255
-    x_va /= 255
-    x_te /= 255
+    
+    #Normalise data by calculating the mean and standard deviation for the 
+    #training data, and transform the training and test data using this
+    mean_value=np.mean(x_tr)
+    std_value=np.std(x_tr)
+    x_tr = (x_tr.astype('float32')-mean_value)/std_value
+    x_va = (x_va.astype('float32')-mean_value)/std_value
+    x_te = (x_te.astype('float32')-mean_value)/std_value
+#    x_tr /= 255
+#    x_va /= 255
+#    x_te /= 255
         
     #%%
     # Create model 
